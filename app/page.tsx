0e5a7fdc5741a99427d33b606769a69f803289ee
Ahmed Yosry <amu3dev@gmail.com>
2024-08-29 21:13:45 +0300

    feat: Add lazy loading for images and update home page
    - Add lazy loading for images to improve page load performance
    - Update home page to include ProductCard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
+import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
     <main>
       <h1>hello world!</h1>
       <Link href="/users">Users</Link>
+      <br />
       <Link href="/users/new">New User</Link>
+      <br />
+      <ProductCard />
+      {/* <Link href="/products">Products</Link> */}
+      <br />
     </main>
   );
 }
