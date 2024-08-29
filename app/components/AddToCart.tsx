1e74663af39eb3b229b454953eed053ddd7b5117
Ahmed Yosry <amu3dev@gmail.com>
2024-08-29 21:12:56 +0300

    feat: Add AddToCart component for buying products

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,19 @@
+'use client'
+import React from "react";
+
+
+const AddToCart = () => {
+  return (
+    <div>
+      {" "}
+      <button
+        onClick={() => {
+          console.log("Buy button clicked");
+        }}>
+        Buy B
+      </button>
+    </div>
+  );
+};
+
+export default AddToCart;
